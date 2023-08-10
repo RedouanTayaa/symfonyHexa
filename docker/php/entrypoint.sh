da7d63e71514b9d90c7d14c081a8a936d9ca4607
Redouan <redouan.tayaa@gmail.com>
2023-08-10 14:33:40 +0200

    first commit

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Installation des dépendances avec Composer
+composer install
+
+# Exécution des migrations Symfony
+#php bin/console doctrine:migrations:migrate --no-interaction
+
+# Lancement du serveur PHP-FPM
+php-fpm
